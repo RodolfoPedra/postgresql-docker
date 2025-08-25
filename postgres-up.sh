637b18dcef8a9c677e2a8441e24a9076961c150c
Rodolfo Pedra <rodolfoaldrovandip@gmail.com>
2025-08-24 22:32:27 -0300

    chore: correção de nome da stack

--- postgres-up.sh
+++ postgres-up.sh
@@ -2,5 +2,5 @@ docker compose -f docker-compose-swarm-server.yml build &&
 echo "imagem buildada para execução" &&
 docker stack rm postgres &&
 echo "removido qualquer stack de postgres no ambiente docker" &&
-docker stack deploy --compose-file=docker-compose-swarm-server.yml nginx &&
+docker stack deploy --compose-file=docker-compose-swarm-server.yml postgres &&
 echo "realizado deploy da stack docker swarm"
