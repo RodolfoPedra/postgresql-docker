97d89f64411b6985633e4a442a3bf78e8367803e
Rodolfo Pedra <rodolfoaldrovandip@gmail.com>
2025-08-24 22:23:05 -0300

    chore: renomeação de docker em sh file

--- postgres-up.sh
+++ postgres-up.sh
@@ -1,6 +1,6 @@
-docker compose -f docker-compose-server.yml build &&
+docker compose -f docker-compose-swarm-server.yml build &&
 echo "imagem buildada para execução" &&
 docker stack rm postgres &&
 echo "removido qualquer stack de postgres no ambiente docker" &&
-docker stack deploy --compose-file=docker-compose-server.yml nginx &&
+docker stack deploy --compose-file=docker-compose-swarm-server.yml nginx &&
 echo "realizado deploy da stack docker swarm"
